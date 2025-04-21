5654df515252ad8235432d06291e42cdc6b406cf
Aloshes <52298691+Aloshes@users.noreply.github.com>
2025-04-22 01:29:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -102,4 +102,4 @@ def home():
     return 'Brain Dump Buddy is running'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT',5000)))
